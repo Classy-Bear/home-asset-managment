13cbe1ccd912156e458f475b0aeafb22ba18d4c6
David <github.6v6mg@passmail.net>
2025-03-05 18:47:57 -0400

    Implement HydratedCubit for persistent state management in HomeCubit
    - Extended HomeCubit to use HydratedCubit for state persistence
    - Added fromJson and toJson methods to enable state serialization
    - Configured main.dart to initialize HydratedStorage for cross-platform support

--- lib/cubits/home/home_cubit.dart
+++ lib/cubits/home/home_cubit.dart
@@ -2,10 +2,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:home_asset_managment/domain/models/asset/asset.dart';
 import 'package:home_asset_managment/domain/models/home/home.dart';
 import 'package:home_asset_managment/domain/repositories/home_repository.dart';
-import 'package:collection/collection.dart'; 
+import 'package:collection/collection.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'home_state.dart';
 
-class HomeCubit extends Cubit<HomeState> {
+class HomeCubit extends HydratedCubit<HomeState> {
   final HomeRepository homeRepository;
 
   HomeCubit({required this.homeRepository})
@@ -13,6 +14,33 @@ class HomeCubit extends Cubit<HomeState> {
 
   Home? getHome(int id) {
     return state.homes.firstWhereOrNull((home) => home.id == id);
+  }
+
+  @override
+  HomeState? fromJson(Map<String, dynamic> json) {
+    try {
+      final homes = (json['homes'] as List)
+          .map((homeJson) => Home.fromJson(homeJson as Map<String, dynamic>))
+          .toList();
+      final selectedHome = json['selectedHome'] as int?;
+      final fetchStatus = FetchStatus.values[json['fetchStatus'] as int];
+      return HomeState(
+        homes: homes,
+        selectedHome: selectedHome,
+        fetchStatus: fetchStatus,
+      );
+    } catch (_) {
+      return null;
+    }
+  }
+
+  @override
+  Map<String, dynamic> toJson(HomeState state) {
+    return {
+      'homes': state.homes.map((home) => home.toJson()).toList(),
+      'selectedHome': state.selectedHome,
+      'fetchStatus': state.fetchStatus.index,
+    };
   }
 
   Future<void> loadHomes() async {
